420e3e5f77d85756c71f3243aecd94d2014e05e2
Brett Viren <brett.viren@gmail.com>
2019-02-20 14:33:18 -0500

    Add support to build older versions to example installdeps.sh script.
    [ci skip]

--- installdeps.sh
+++ installdeps.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-set -e
 
 insdir="$(readlink -f $1)"; shift
 blddir="$(readlink -f $1)"; shift
+options="$1" ; shift
+
+set -e
+
+libzmq_version="4.3.1"
+libzmq_repo="libzmq"
+czmq_version="4.2.0"
+
+protobuf_version="3.6.1"
+protobuf_url="https://github.com/protocolbuffers/protobuf/releases/download/v${protobuf_version}/protobuf-cpp-${protobuf_version}.tar.gz"
+
+if [[ "$options" =~ .*legacy.* ]] ; then
+    libzmq_version="3.2.5"
+    libzmq_repo="zeromq3-x"
+    czmq_version="3.0.2"
+    protobuf_version="3.3.1"
+    protobuf_url="https://github.com/protocolbuffers/protobuf/archive/v3.3.1.tar.gz"
+fi
+
+force () {
+    if [[ "$options" =~ .*force.* ]] ; then
+        echo yes
+    fi
+}
 
 if [ ! -d "$blddir" ] ; then
     mkdir -p "$blddir"
@@ -11,13 +34,13 @@ fi
 pushd "$blddir"
 
 PATH="$insdir/bin:$PATH"
-LD_LIBRARY_PATH="$insdir/lib:$LD_LIBRARY_PATH"
-PKG_CONFIG_PATH="$insdir/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$insdir/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="$insdir/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 download () {
     local url="$1"; shift
     local fname="$(basename $url)"
-    if [ -f "$fname" ] ; then
+    if [ -f "$fname" -a -z "$(force)" ] ; then
         echo "Already have $fname"
         return
     fi
@@ -26,15 +49,21 @@ download () {
 
 install-libzmq () {
     if pkg-config libzmq ; then
-        echo "have libzmq"
-        return
+        if [ -z "$(force)" ] ; then
+           echo "have libzmq"
+           return
+        fi
     fi
+    # https://github.com/zeromq/zeromq3-x/releases/download/v3.2.5/zeromq-3.2.5.tar.gz
+    local name="zeromq-${libzmq_version}"
+    local fname="${name}.tar.gz"
+    local url="https://github.com/zeromq/${libzmq_repo}/releases/download/v${libzmq_version}/${fname}"
 
-    download https://github.com/zeromq/libzmq/releases/download/v4.3.1/zeromq-4.3.1.tar.gz
-    if [ ! -d zeromq-4.3.1 ] ; then
-        tar -xf zeromq-4.3.1.tar.gz
+    download "$url"
+    if [ ! -d "$name" -o -n "$(force)" ] ; then
+        tar -xf "$fname"
     fi
-    pushd zeromq-4.3.1
+    pushd "$name"
     ./configure --prefix="$insdir"
     make && make install
     popd
@@ -42,15 +71,20 @@ install-libzmq () {
 
 install-libczmq () {
     if pkg-config libczmq ; then
-        echo "have libczmq"
-        return
+        if [ -z "$(force)" ] ; then
+            echo "have libczmq"
+            return
+        fi
     fi
 
-    download https://github.com/zeromq/czmq/releases/download/v4.2.0/czmq-4.2.0.tar.gz
-    if [ ! -d czmq-4.2.0 ] ; then
-        tar -xf czmq-4.2.0.tar.gz
+    local name="czmq-${czmq_version}"
+    local fname="${name}.tar.gz"
+    local url="https://github.com/zeromq/czmq/releases/download/v${czmq_version}/${fname}"
+    download "$url"
+    if [ ! -d "$name" -o -n "$(force)" ] ; then
+        tar -xf "$fname"
     fi
-    pushd czmq-4.2.0
+    pushd "$name"
     ./configure --prefix="$insdir"  --with-pkgconfigdir="$PKG_CONFIG_PATH"
 
     make && make install
@@ -64,17 +98,25 @@ install-libczmq () {
 
 install-protobuf () {
     if pkg-config protobuf ; then
-        echo "have protobuf"
-        return
+        if [ -z "$(force)" ] ; then
+            echo "have protobuf"
+            return
+        fi
     fi
 
     # apt-get install -y uuid-dev libsystemd-dev liblz4-dev libcurl4-nss-dev libzmq5-dev libczmq-dev
 
-    download https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
-    if [ ! -d protobuf-3.6.1 ] ; then
-        tar -xf protobuf-cpp-3.6.1.tar.gz
+    local name="protobuf-${protobuf_version}"
+    local fname="$(basename $protobuf_url)"
+
+    download "$protobuf_url"
+    if [ ! -d "$name" -o -n "$(force)" ] ; then
+        tar -xf "$fname"
     fi
-    pushd protobuf-3.6.1
+    pushd "$name"
+    if [[ "$options" =~ .*legacy.* ]] ; then
+        ./autogen.sh
+    fi
     ./configure --prefix="$insdir"
     make && make install
     popd
