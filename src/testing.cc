54dfe1f90f78105ff129514ce882d1f5192f50d6
Brett Viren <brett.viren@gmail.com>
2019-03-02 12:43:29 -0500

    Fix #3.

--- src/testing.cc
+++ src/testing.cc
@@ -9,11 +9,51 @@
 
 using json = nlohmann::json;
 
+const int64_t K = 1000;
+const int64_t M = 1000*K;
+const int64_t G = 1000*M;
+
+namespace units {
+    const int64_t ns = 1;
+    const int64_t us = 1000*ns;
+    const int64_t ms = 1000*us;
+    const int64_t s = 1000*ms;
+}
 
 void ptmp::testing::init(ptmp::data::TPSet& tps)
 {
     tps.set_count(0);
     tps.set_detid(4); // guaranteed random by roll of dice, but call only once
+}
+
+void ptmp::testing::set_timestamp(ptmp::data::Timestamp& ts, int64_t t, int64_t latency)
+{
+    if (t == 0) {
+        t = zclock_usecs() * units::us;
+    }
+
+    t += latency;
+
+    ts.set_seconds(t/G);
+    ts.set_nanosecs(t%G);
+}
+
+int64_t ptmp::testing::timestamp_span(const ptmp::data::Timestamp& ta,
+                                      const ptmp::data::Timestamp& tb)
+{
+    const int64_t s = ta.seconds() - tb.seconds();
+    const int64_t ns = ta.nanosecs() - tb.nanosecs();
+    return s*G + ns;
+}
+void ptmp::testing::set_relative_timestamp(ptmp::data::Timestamp& ts,
+                                           const ptmp::data::Timestamp& t0,
+                                           int64_t delay)
+{
+    const int64_t full_ns = t0.nanosecs() + delay%G;
+    const int64_t s = t0.seconds() + (full_ns + delay)/G;
+    const int32_t ns = full_ns % G;
+    ts.set_seconds(s);
+    ts.set_nanosecs(ns);    
 }
 
 void ptmp::testing::set_count_clock(ptmp::data::TPSet& tps)
@@ -22,23 +62,23 @@ void ptmp::testing::set_count_clock(ptmp::data::TPSet& tps)
     const int oldcount = tps.count();
     tps.set_count(oldcount + 1);
 
-    const int64_t M = 1000000;
-    const int64_t us = zclock_usecs();
-    const int64_t s = us/M;
-    const int64_t ns = (us%M) * 1000;
+    // The "data time" is some electronics hardware clock shared in
+    // some way by all messages.  Whatever units it may be in, we
+    // shall store the time in an absolute number of seconds from the
+    // Unix epoch and, if applicable, the number of nanoseconds within
+    // the second.  Here, we just plop ZMQ's monotonic system clock
+    // number and we fake some latency by moving it into the past a
+    // little bit.
 
+    // We fake some latency between data clock and wall clock by
+    // setting the tstart a bit before wall clock's "now".
+    auto tstart = tps.mutable_tstart();
+    set_timestamp(*tstart, 0, -100*units::us);
 
-    // normally, "data time" is some electronics hardware clock shared
-    // in some whay by all messages.  It is not necessarily expressed
-    // in units of seconds/us/ns.  It may be relative to some global
-    // DAQ initialization time.  Here, we just plop ZMQ's monotonic
-    // system clock number and we fake some latency by moving it into
-    // the past a bit.
-    tps.set_datatime(us - 100);       
-
-    // system clock 
-    tps.set_unixseconds(s);
-    tps.set_unixns(ns);
+    // Set the created time which really should be the system wall
+    // clock "now".  No fake here.
+    auto created = tps.mutable_created();
+    set_timestamp(*created);
 }
 
 
@@ -51,6 +91,8 @@ void ptmp::testing::make_tps_t::operator()(ptmp::data::TPSet& tps) {
     tps.clear_tps();
     ptmp::testing::set_count_clock(tps);
     int ntps = std::max(0, (int)normal_dist(generator));
+    const auto& t0 = tps.tstart();
+    ptmp::data::Timestamp tend;
     for (int ind=0; ind<ntps; ++ind) {
         auto tp = tps.add_tps();
 
@@ -59,19 +101,23 @@ void ptmp::testing::make_tps_t::operator()(ptmp::data::TPSet& tps) {
         // particular, zeros are avoided so that protobuf compression
         // doesn't give an unrealistic size.
 
-        const int tspan = (1+ind)*10;
+        const int tspan = ((1+ind)*10/2)*units::us;
         const int adc = ind+10;
 
         // required values
         tp->set_channel(ind);
-        tp->set_tstart(ind*100);
-        tp->set_tspan(tspan);
+        auto tstart = tp->mutable_tstart();
+        set_relative_timestamp(*tstart, t0, ind*10*units::us);
+
+        set_relative_timestamp(tend, *tstart, tspan);
 
         // optional values
         tp->set_adcsum(adc*tspan/2);
         tp->set_adcpeak(ind/tspan);
         tp->set_flags(0);
     }
+
+    tps.set_tspan(timestamp_span(t0, tend));
 }
 
 ptmp::testing::uniform_sleeps_t::uniform_sleeps_t(int t, int s)
