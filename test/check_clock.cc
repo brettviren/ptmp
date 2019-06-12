c79158e209528bb8d71940deeae980add66160c3
Brett Viren <brett.viren@gmail.com>
2019-06-12 18:37:01 -0400

    Add chrono

--- test/check_clock.cc
+++ test/check_clock.cc
@@ -1,15 +1,23 @@
-
+#include <chrono>
 #include <czmq.h>
 #include <ctime>
-
+using namespace std::chrono;
 int main()
 {
     zsys_init();
-    zsys_info("time:            %ld", time(0));
-    zsys_info("zclock_time:     %ld", zclock_time());
-    zsys_info("zclock_mono:     %ld", zclock_mono());
-    zsys_info("zclock_usecs:    %ld", zclock_usecs());
-    zsys_info("time-ztime/1000: %ld", time(0)- zclock_time()/1000);
-    zsys_info("time-zmono/1000: %ld", time(0)- zclock_mono()/1000);
+    zsys_info("time:                  %ld", time(0));
+    zsys_info("zclock_time:           %ld", zclock_time());
+    zsys_info("zclock_mono:           %ld", zclock_mono());
+    zsys_info("zclock_usecs:          %ld", zclock_usecs());
+
+    
+    system_clock::time_point sys_tp = system_clock::now();
+    system_clock::duration sys_dtn = sys_tp.time_since_epoch();
+    zsys_info("chrono system periods: %ld", sys_dtn.count());
+
+    high_resolution_clock::time_point hrc_tp = high_resolution_clock::now();    
+    system_clock::duration hrc_dtn = hrc_tp.time_since_epoch();    
+    zsys_info("chrono hirez periods:  %ld", hrc_dtn.count());
+
     return 0;
 }
