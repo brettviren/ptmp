bc1a3ea99068ec8e3e94d9c0af814a0d88403a68
Brett Viren <brett.viren@gmail.com>
2019-07-11 13:46:10 -0400

    Remove in order to add as submodule

--- waftools/czmq.py
+++
@@ -1,9 +0,0 @@
-import generic
-
-def options(opt):
-    generic._options(opt, 'czmq')
-
-def configure(cfg):
-    generic._configure(cfg, 'czmq', incs=('czmq.h',), libs=('czmq',),
-                       pcname='libczmq',
-                       uses = 'LIBZMQ', mandatory=True)
--- waftools/libzmq.py
+++
@@ -1,8 +0,0 @@
-import generic
-
-def options(opt):
-    generic._options(opt, 'libzmq')
-
-def configure(cfg):
-    generic._configure(cfg, 'libzmq', incs=('zmq.h',), libs=('zmq',),
-                       mandatory=True)
