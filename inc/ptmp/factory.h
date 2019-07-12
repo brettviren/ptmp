ef1395614cd73dea94f766065522562e0a8de130
Brett Viren <brett.viren@gmail.com>
2019-07-12 10:51:51 -0400

    Forgot to add this header

---
+++ inc/ptmp/factory.h
@@ -0,0 +1,26 @@
+#ifndef PTMP_FACTORY
+#define PTMP_FACTORY
+
+#include "ptmp/api.h"
+#include "ptmp/upif.h"
+
+namespace ptmp {
+
+    class AgentFactory {
+        upif::cache m_cache;
+    public:
+        AgentFactory();
+        ~AgentFactory();
+        ptmp::TPAgent* make(const std::string& alias, const std::string& config);
+
+        upif::cache& plugin_cache() { return m_cache; }
+        
+    };
+
+}
+
+#define PTMP_AGENT(TYPE,ALIAS) \
+extern "C" { void* ptmp_make_##ALIAS##_agent(const char* config) { return new TYPE(config); } }
+
+
+#endif 
